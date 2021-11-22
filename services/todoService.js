c2c2a27259eae35bec39afea0091ff568eb834a3
syadav-deqode <syadav@deqode.com>
2021-11-22 18:06:03 +0530

    New APIs service.

---
+++ services/todoService.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const sequelize = require('../helpers/db')
+const todoRepository = require('../repositories/todoRepository')
+
+module.exports.addTodo = async (body) => {
+  let returnObj = { error: null, success: null, message: null, }
+
+  const { todo } = body
+  // Check for duplicate records
+  const record = await todoRepository.getTodoByName(todo)
+
+  if (!record) {
+    // Add new
+    const payload = { todo }
+    await sequelize.transaction(async (t) => {
+      await todoRepository.addNewTodo(payload, { t })
+    })
+
+    returnObj = { ...returnObj, success: true, message: "New todo added", }
+  } else {
+    // Return error to notify the record is exists
+    returnObj = { ...returnObj, error: true, message: "Todo already in use" }
+  }
+
+  return { todo: returnObj }
+}
+
+module.exports.getTodos = async () => {
+  const { todos } = await todoRepository.getAllTodos()
+  return { todos }
+}

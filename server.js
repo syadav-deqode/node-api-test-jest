bde18b91c2d7de0c45d5a1b63dba918e6a4687a3
syadav-deqode <syadav@deqode.com>
2021-11-22 17:44:12 +0530

    DB, response middleware and routes referance added.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser')
+const responseMw = require('./middlewares/responseMw.js')
 const port = process.env.PORT || 5500;
 
 const app = express();
@@ -10,6 +11,16 @@ global.__base = __dirname;
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Db connection
+require('./helpers/db')
+app.get('/', (req, res) => { res.send("Hello World!") });
+
+// routes
+app.use('/todos', require('./routes/todo'))
+
+// Pass app to the response middleware
+responseMw(app)
 
 app.listen(port, () => console.log(`Server startted at the port ${port}`))
 
